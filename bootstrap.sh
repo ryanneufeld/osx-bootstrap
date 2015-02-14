a6f3884eb93650381663e5b884a68485eb2595f6
Chris Murray <chris@ipstuff.ca>
2015-02-13 22:28:35 -0800

    cleanup, order of ops changes

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,42 @@
 #!/usr/bin/env bash
+##
+## OSX host bootsrap. Installs ansible, then homebrew, then runs our playbook
+## to bootstrap the local machine. hosts includes the local machine only.
+##
+## my installed items are included, it is network engineer and security focused.
+##
+## questions, comments, concerns: chris[at]ipstuff.ca
+##
+##
+
 set -e
 
+## install pip with easy install.
+##
 echo 'Installing Ansible via pip'
-
 if [[ ! -x `which pip` ]]; then
     sudo easy_install pip
 fi
 
+## set CLFAGS and CPPFLAGS, needed for some installs.
+##
 if [[ -x `which pip` && ! -x `which ansible` ]]; then
     sudo CFLAGS=-Qunused-arguments CPPFLAGS=-Qunused-arguments pip install ansible
 fi
 
+## Install homebrew if it does not exist
+##
+if [[ ! -x `which homebrew` ]]; then
+    /usb/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    # after lets install caskroom too!
+fi
+
+## install caskroom
+##
+brew install caskroom/cask/brew-cask
+
+## after all else, run ansible.
+##
 echo 'Running Ansible... see you on the other side'
 
 if [[ -x `which ansible` ]]; then
